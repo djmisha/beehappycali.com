b5b7e3acafc4fd55b5312a285dcbc873f8d272b5
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2022-10-26 07:21:23 -0700

    bee funcs

--- src/components/Bee.js
+++ src/components/Bee.js
@@ -1,16 +1,54 @@
-import React from "react";
-import ConfettiMaker from "./ConfettiMaker";
+import React, { useEffect, useState } from 'react';
+import ConfettiMaker from './ConfettiMaker';
+import isBrowser from '../helpers/isBrowser';
+import { navigate } from 'gatsby-link';
 
 const Bee = () => {
-  const handleBees = () => {
-    ConfettiMaker(['😍', '🥰', '❤️']);
-  }
+  let [classes, setClassName] = useState('bee top');
+  let [message, setMessage] = useState();
+  let [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    if (isBrowser()) {
+      window.addEventListener('scroll', () => {
+        let distance = window.pageYOffset;
+        setOffset(distance);
+        handleScroll(distance);
+      });
+    }
+  }, []);
+
+  const handleScroll = (distance) => {
+    if (distance > 200) {
+      setClassName('bee bottom');
+      setMessage("What's Buzzin' ?");
+    }
+    if (distance < 200) {
+      setClassName('bee top');
+      setMessage('');
+    }
+  };
+
+  const handleClick = () => {
+    if (offset > 200) {
+      navigate('#Contact');
+      ConfettiMaker(['🐝']);
+    }
+    if (offset < 200) {
+      ConfettiMaker(['😍', '🥰', '❤️']);
+    }
+  };
 
   return (
-    <button className="bee" onClick={handleBees}>
-      🐝 
+    <button className={classes} onClick={handleClick} onScroll={handleScroll}>
+      {message && (
+        <div className="chat-bubble">
+          <p>{message}</p>
+        </div>
+      )}
+      <div className="happy-bee">🐝</div>
     </button>
-  )
+  );
 };
 
-export default Bee
+export default Bee;
